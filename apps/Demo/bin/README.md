4b30027697b2d4ad4fe228a3f7b17167789df94a
whmbuaa <whm_buaa@hotmail.com>
2015-09-15 14:52:56 +0800

    add demo app.

---
+++ apps/Demo/bin/README.md
@@ -0,0 +1,2 @@
+# product-projects
+product projects to quickly create an android app
---
+++ apps/Demo/src/com/quick/demo/MainActivity.java
@@ -0,0 +1,88 @@
+package com.quick.demo;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import com.alibaba.fastjson.JSON;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.quick.framework.network.http.GetHomeArticleListRequest;
+import com.quick.framework.network.http.GetHomeArticleListRequest.RequestResult;
+
+
+public class MainActivity extends Activity {
+
+	private TextView mTvMessage;
+	private RequestQueue mRequestQueue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        mTvMessage = (TextView)findViewById(R.id.message);
+        mRequestQueue = Volley.newRequestQueue(this);
+        
+        findViewById(R.id.get_home_list).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Request<GetHomeArticleListRequest.RequestResult> request = new GetHomeArticleListRequest(true, true, 10, new Listener<GetHomeArticleListRequest.RequestResult>(){
+
+					@Override
+					public void onResponse(RequestResult param) {
+						// TODO Auto-generated method stub
+						Log.i("quick",JSON.toJSONString(param));
+						mTvMessage.setText(JSON.toJSONString(param));
+						
+					}},
+					new ErrorListener(){
+
+						@Override
+						public void onErrorResponse(VolleyError paramVolleyError) {
+							// TODO Auto-generated method stub
+							Log.i("quick",paramVolleyError.getMessage());
+							mTvMessage.setText(paramVolleyError.getMessage());
+						}});
+				mRequestQueue.add(request);
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onDestroy() {
+    	// TODO Auto-generated method stub
+    	super.onDestroy();
+    	mRequestQueue.cancelAll(this);
+    }
+}
